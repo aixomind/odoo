c219c600df7c1bdd7f4ac33cbd3d584f9be38581
Krishnaraj-L4E <krishnaraj@links4engg.com>
2026-07-22 15:37:49 +0530

    Added the dashboard collection modules

---
+++ l4e_dashboard_collection_19/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from . import res_config_settings
+from . import sale_order
+from . import inventory_dashboard
+from . import product_category
+from . import financial_dashboard
+from . import amc_dashboard
