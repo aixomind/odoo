2e94262213b265db70e59c23d1720f608bd647ce
surya-krishna-L4E <surya@links4engg.com>
2026-09-08 14:18:09 +0530

    price update

--- l4e_access_management_pro/__manifest__.py
+++ l4e_access_management_pro/__manifest__.py
@@ -24,6 +24,8 @@
     'name': 'Simplify & Advanced Access Management Pro',
     'version': '19.0.1.0.0',
     'category': 'Tools/Administration',
+    'price': 150,
+    'currency': 'USD',
     'summary': 'All-in-one Access Management solution to restrict Menus, Models, Fields, Buttons, Tabs, Views, Chatter, Reports, Export/Import, and Debug Mode.',
     'description': """
 Access Management Pro for Odoo 19
