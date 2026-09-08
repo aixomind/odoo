69315261829dc5bcd7c0303779652fadd79c17e8
surya-krishna-L4E <surya@links4engg.com>
2026-09-08 14:19:21 +0530

    Update __manifest__.py

--- l4e_access_management_pro/__manifest__.py
+++ l4e_access_management_pro/__manifest__.py
@@ -24,7 +24,7 @@
     'name': 'Simplify & Advanced Access Management Pro',
     'version': '19.0.1.0.0',
     'category': 'Tools/Administration',
-    'price': 150,
+    'price': 150.00,
     'currency': 'USD',
     'summary': 'All-in-one Access Management solution to restrict Menus, Models, Fields, Buttons, Tabs, Views, Chatter, Reports, Export/Import, and Debug Mode.',
     'description': """
